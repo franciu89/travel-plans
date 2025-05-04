2658f7245938d6675960dd11bdcd18099fcaf8f4
franciu89 <francesca7bonini@gmail.com>
2025-05-04 11:22:10 +0000

    Refactor layout and configuration files; enhance Tokyo itinerary with components and styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-    server: {
-        host: '127.0.0.1'
-    },
-    integrations: [mdx()]
+  server: {
+    host: "127.0.0.1",
+  },
+  integrations: [mdx()],
+  site: "https://franciu89.github.io",
+  base: "/travel-plans",
+  trailingSlash: "ignore",
 });
